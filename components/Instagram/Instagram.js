4270142611c54b66b312afd6ca6ba7d1ae6856be
Burak Kaplan <burakkaplanwork@gmail.com>
2022-03-10 15:01:23 +0100

    fixing bugs

--- components/Instagram/Instagram.js
+++ components/Instagram/Instagram.js
@@ -5,13 +5,23 @@ import StoryUpload from "./StoryUpload";
 import UploadInstagram from "./UploadInstagram";
 import StoryModalComponent from "./StoryModalComponent";
 import Suggestions from "./Suggestions";
+import { useSession } from "next-auth/react";
 const Instagram = () => {
+  const { data: session } = useSession();
   return (
     <div className="dark:bg-gray-900">
       <HeaderInsta />
 
       <div className="grid lg:grid-cols-5 lg:space-x-5 justify-evenly pt-10 mx-auto w-[70%]">
         <div className="lg:col-span-3 h-[100vh] overflow-y-scroll">
+          {session && (
+            <>
+              <p>{session.user.name}</p>
+              <p>{session.user.email}</p>
+              <p>{session.user.uid}</p>
+              <p>{session.user.username}</p>
+            </>
+          )}
           <Stories />
           <Posts />
           <UploadInstagram />
