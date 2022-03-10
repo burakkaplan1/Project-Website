1652d8668f4b760ec2072bd46fa5f734211a6eeb
Burak Kaplan <72610852+burakkaplan1@users.noreply.github.com>
2022-03-10 13:24:51 +0100

    Adding Facebook Provider to [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -9,13 +9,13 @@ export default NextAuth({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
- //   FacebookProvider({
- //     clientId: process.env.FACEBOOK_CLIENT_ID,
- //     clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-//    }),
+   FacebookProvider({
+    clientId: process.env.FACEBOOK_CLIENT_ID,
+    clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+   }),
   
   ],
-    secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.NEXTAUTH_SECRET,
   pages: {
     signIn: "/auth/signin",
   },
