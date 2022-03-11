a202f2ac0b2ab63462552e6da8afb6f528d9cf96
Burak Kaplan <burakkaplanwork@gmail.com>
2022-03-11 19:00:36 +0100

    Adding GitHub provider for Authentication

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import FacebookProvider from "next-auth/providers/facebook";
+import GitHubProvider from "next-auth/providers/github";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -12,6 +13,10 @@ export default NextAuth({
     FacebookProvider({
       clientId: process.env.FACEBOOK_CLIENT_ID,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
     // ...add more providers here
   ],
